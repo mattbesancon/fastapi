70859cf46dc748f63f2f8cf6952877421c75e352
Matthieu Besancon <matthieu.besancon@gmail.com>
2022-06-14 11:05:18 +0200

    schema validation with pydantic

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+from hashlib import new
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def read_root():
     return {"hello": "welcome to my api"}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
+def create_posts(new_post: Post):
+    print(dict(new_post))
     return {
-        "new_post": f"title {payload['title']} content {payload['content']}"
+        "data": new_post
     }
 
