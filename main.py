b78171ac44c911db26312bb8bf4342ffd994cf71
Matthieu Besancon <matthieu.besancon@gmail.com>
2022-06-15 11:41:22 +0200

    create post with manual id generated

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -12,14 +13,18 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title": "title 1", "content": "content 1", "id": 1}, {"title": "title 2", "content": "content 2", "id": 2}]    
+
 @app.get("/")
 def read_root():
-    return {"hello": "welcome to my api"}
+    return {"data": my_posts}
 
 @app.post("/posts")
 def create_posts(post: Post):
-    print(dict(post))
+    post_dict = (dict(post))
+    post_dict["id"] = randrange(10000000)
+    my_posts.append(post_dict)
     return {
-        "data": post
+        "data": post_dict
     }
 
