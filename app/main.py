8fa022c1a26dd2b6ed0c58404a1021da33aaf050
Matthieu Besancon <matthieu.besancon@gmail.com>
2022-07-22 17:29:01 +0200

    import passlib & hash password when creating user with secret key generated via openssl hex

--- app/main.py
+++ app/main.py
@@ -8,10 +8,30 @@ import psycopg2
 from . import models, schema
 from .database import SessionLocal, engine
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+
 
 models.Base.metadata.create_all(bind=engine)
 
+
+SECRET_KEY = "4dee5112e82b454ea63ceecc9e50a333bd0a55f3eb97858098ef9a20841d5e0c"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
 app = FastAPI()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
 
 def get_db():
     db = SessionLocal()
@@ -53,6 +73,7 @@ def create_posts(post: schema.PostCreate, db: Session = Depends(get_db)):
     return {
         "data": db_post
     }
+
 
 @app.get("/posts/{id}", response_model=schema.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
@@ -97,7 +118,7 @@ def update_post(id: int, post: schema.PostCreate, db: Session = Depends(get_db))
 
 @app.post("/users", status_code=201)
 def create_users(user: schema.UserCreate, db: Session = Depends(get_db)):
-    db_user = models.User(email=user.email, password=user.password)
+    db_user = models.User(email=user.email, password=get_password_hash(user.password))
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
