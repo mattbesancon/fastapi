f3c856d68c4ac935ebf6af8f889e5a3e18b8d16a
Matthieu Besancon <matthieu.besancon@gmail.com>
2022-08-04 16:05:11 +0200

    2 bugs in oauth2 with utcnow & algo in brackets in get_current_user method

--- app/oauth2.py
+++ app/oauth2.py
@@ -14,7 +14,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict):
     to_encode = data.copy()    
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
 
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -29,7 +29,7 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         )
 
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithm=ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
 
         id = payload.get("user_id")
 
